e49b4daab49ab17a423972e2a292181332db74c3
Mequidi <nikhilsejwal234@gmail.com>
2021-08-18 17:01:10 +0530

    changed navbar

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -4,6 +4,7 @@ import { AiOutlineHome } from 'react-icons/ai'
 import { IoIosPerson } from 'react-icons/io'
 import { AiOutlineFundProjectionScreen } from 'react-icons/ai'
 import { CgFileDocument } from 'react-icons/cg'
+import { FaBars } from 'react-icons/fa'
 import { useState } from "react";
 import React from "react";
 import { useEffect, useRef } from "react/cjs/react.development";
@@ -30,7 +31,7 @@ const Navbar = (props) =>{
                     <div className={classes["logo-container"]}>
                         <img className={classes.logoimg} src={logoImg} alt="logo"/>
                     </div>
-                    <button className={classes.togglebtn}></button>
+                    <button className={classes.togglebtn}><FaBars /></button>
                     <div className={classes["list-container"]}>
                         <ul className={classes.list}>
                             <li className={classes["list-item"]} onClick={()=>{
